023394030761567729f9349d6ddcb92784104e5c
user1 <user1@163.com>
2019-07-10 09:21:16 +0800

    create view login

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
-der index(request):
+def index(request):
 	return HttpResponse("index")
+
+def login(request):
+	return redirect("/index")
